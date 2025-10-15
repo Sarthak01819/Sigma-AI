2f2d8c0e65aee6eb2e0d12b635ee9deb0f6d119d
Sarthak01819 <146155287+Sarthak01819@users.noreply.github.com>
2025-10-15 12:07:33 +0530

    Clerk Authentication Added & Updated Footer

--- app/_components/AppSidebar.jsx
+++ app/_components/AppSidebar.jsx
@@ -8,13 +8,16 @@ import {
     SidebarGroup,
     SidebarHeader,
 } from "@/components/ui/sidebar"
-import { Moon, Sun } from "lucide-react"
+import { SignInButton, useUser } from "@clerk/nextjs";
+import { Bolt, Moon, Sun, User2, Zap } from "lucide-react"
 import { useTheme } from "next-themes";
 import Image from "next/image"
+import UsageCreditProgress from "./UsageCreditProgress";
 
 export function AppSidebar() {
 
     const { theme, setTheme } = useTheme();
+    const { user } = useUser();
 
     return (
         <Sidebar>
@@ -30,19 +33,35 @@ export function AppSidebar() {
                                 : <Button onClick={() => setTheme('light')}><Moon /></Button>}
                         </div>
                     </div>
-                    <Button className="w-full mt-4">+ New Chat</Button>
+                    {user ? <Button className="w-full mt-4">+ New Chat</Button> :
+                    <SignInButton mode="modal">
+                        <Button className={"w-full mt-4 cursor-pointer"}>Sign In to start</Button>
+                    </SignInButton>}
                 </div>
             </SidebarHeader>
             <SidebarContent>
                 <SidebarGroup>
                     <div className='p-3'>
                         <h2 className="font-bold text-lg">Chat</h2>
-                        <p className="text-sm text-gray-400">Sign in  to start chating with multiple AI models </p>
+                        {!user&&<p className="text-sm text-gray-400">Sign in  to start chating with multiple AI models </p>}
                     </div>
                 </SidebarGroup>
             </SidebarContent>
             <SidebarFooter>
-                <Button className="w-full mb-4">Sign In / Sign Up</Button>
+                {!user ? <SignInButton mode="modal">
+                    <Button className={"w-full mb-4 cursor-pointer"}>Sign In / Sign Up</Button>
+                </SignInButton>
+                    :
+                    <div>
+                        <UsageCreditProgress />
+                        <Button className={'flex my-4 w-full'} variant={''}>
+                            <Zap /><h2>Upgrade to plus</h2>
+                        </Button>
+                        <Button className={'flex w-full my-5'} variant={'ghost'}>
+                            <User2 /><h2>Settings</h2>
+                        </Button>
+                    </div>
+                }
             </SidebarFooter>
         </Sidebar>
     )
